b9ef87472eafed699b74905c01f367c97755bfe5
G-MTI <gaia.mazzanti07@gmail.com>
2025-08-24 17:43:45 +0200

    Added the phases page

--- FAQ.js
+++ FAQ.js
@@ -3,37 +3,37 @@ const cards = document.querySelectorAll('.card');
     const itemsPerPage = 3;
 
 function showCards() {
-  cards.forEach((card, index) => {
-    if (index >= currentIndex && index < currentIndex + itemsPerPage) {
-      card.style.display = "block"; // Mostra
-    } else {
-      card.style.display = "none"; // Nascondi
-    }
-  });
+    cards.forEach((card, index) => {
+        if (index >= currentIndex && index < currentIndex + itemsPerPage) {
+          card.style.display = "block"; // Mostra
+        } else {
+          card.style.display = "none"; // Nascondi
+        }
+    });
 }
 showCards();
 
-//next cart
+//next card
 document.getElementById('next').addEventListener('click', () => {
-  if (currentIndex + itemsPerPage < cards.length) {
-    currentIndex += itemsPerPage; 
-    showCards();
-  }
+    if (currentIndex + itemsPerPage < cards.length) {
+        currentIndex += itemsPerPage; 
+        showCards();
+    }
 });
 
-//pprev cart
+//prev card
 document.getElementById('prev').addEventListener('click', () => {
-  if (currentIndex - itemsPerPage >= 0) {
-    currentIndex -= itemsPerPage; 
-    showCards();
-  }
+    if (currentIndex - itemsPerPage >= 0) {
+        currentIndex -= itemsPerPage; 
+        showCards();
+    }
 });
 
 //serch
 document.getElementById('search').addEventListener('input', (e) => {
-  const query = e.target.value.toLowerCase();
-  cards.forEach(card => {
-    const text = card.innerText.toLowerCase();
-    card.style.display = text.includes(query) ? "block" : "none";
-  });
+    const query = e.target.value.toLowerCase();
+    cards.forEach(card => {
+        const text = card.innerText.toLowerCase();
+        card.style.display = text.includes(query) ? "block" : "none";
+    });
 });
---
+++ phases.js
@@ -0,0 +1,35 @@
+ const menstual = document.getElementById("menstual");
+
+    menstual.addEventListener("mouseover", function() {
+        menstual.style.backgroundColor = "#e63946";
+    });
+    menstual.addEventListener("mouseout", function() {
+        menstual.style.backgroundColor = "#F5F5F5";
+    });
+
+ const follicular = document.getElementById("follicular");
+
+    follicular.addEventListener("mouseover", function() {
+        follicular.style.backgroundColor = "#4CC9F0";
+    });
+    follicular.addEventListener("mouseout", function() {
+        follicular.style.backgroundColor = "#F5F5F5";
+    });
+
+ const ovulation = document.getElementById("ovulation");
+
+    ovulation.addEventListener("mouseover", function() {
+        ovulation.style.backgroundColor = "#FFB703";
+    });
+    ovulation.addEventListener("mouseout", function() {
+        ovulation.style.backgroundColor = "#F5F5F5";
+    });
+
+ const luteal = document.getElementById("luteal");
+
+    luteal.addEventListener("mouseover", function() {
+        luteal.style.backgroundColor = "#9D4EDD";
+    });
+    luteal.addEventListener("mouseout", function() {
+        luteal.style.backgroundColor = "#F5F5F5";
+    });
